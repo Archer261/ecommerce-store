d3135f3266693df3f5c2bd523217d10f34e3d395
Joseph Archer <viewtiful.kja@gmail.com>
2022-10-24 12:16:04 -0400

    updated model routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,51 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const tagData = await Tag.findAll({
+    include: [{ model: Product }]
+  })
+  res.render("tag")
   // find all tags
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  const tagData = await Tag.findOne({
+    where: { id: req.params.id },
+    include: [{ model: Product }]
+  });
+  res.render("tag", {
+
+  })
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
+// create a new tag
 router.post('/', (req, res) => {
-  // create a new tag
+  Tag.create(req.body)
+    .then((tag) => {
+      res.status(200).json(tag);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
   // update a tag's name by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({ where: { id: req.params.id } })
 });
 
 module.exports = router;
